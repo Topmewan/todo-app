caae015269add9b46805af6e8e9d662e0440e0a1
Topmewan <anon8733@gmail.com>
2021-09-20 13:18:57 +0300

    Todo

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import {useState} from "react";
+import TodoForm from "./TodoForm";
+import {RiCloseCircleLine, TiEdit} from "react-icons/all";
+
+function Todo(){
+    const [edit,setEdit] = useState({
+        id:null,
+        value:''
+    })
+
+
+    return todos.map((todo,index) => (
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+             key={index}
+        >
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className="icons">
+                <RiCloseCircleLine/>
+                <TiEdit/>
+
+
+            </div>
+
+        </div>
+
+    ))
+
+}
+
+export default Todo;
